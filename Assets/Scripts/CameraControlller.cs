aee58ca112bb1b371c8e4754f2acd03c5bf9f4ce
Filip Różalski <f.rozalski95@gmail.com>
2025-09-25 21:37:19 +0200

    Added Camera Control

---
+++ Assets/Scripts/CameraControlller.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class CameraControlller : MonoBehaviour
+{
+    public bool clampZoom;
+    Vector3 lastMousePos;
+
+    const float minZoomValue = 5f;
+    const float maxZoomValue = 50f;
+
+    void Update()
+    {
+        CameraDrag();
+        CameraZoom();
+    }
+
+
+    void CameraDrag()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            lastMousePos = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(2))
+        {
+            Vector3 currentMousePos = Input.mousePosition;
+            Vector3 mouseDelta = currentMousePos - lastMousePos;
+
+            float distanceFromCamera = transform.position.y;
+            float worldHeight = 2f * distanceFromCamera * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+            float worldUnitsPerPixel = worldHeight / Screen.height;
+
+            Vector3 worldMovement = new Vector3(
+                -mouseDelta.x * worldUnitsPerPixel,
+                0,
+                -mouseDelta.y * worldUnitsPerPixel
+            );
+            transform.Translate(worldMovement);
+            lastMousePos = currentMousePos;
+        }
+    }
+    void CameraZoom()
+    {
+        transform.Translate(Vector3.down * Input.mouseScrollDelta.y * (transform.position.y / 10));
+        if (clampZoom)
+        {
+            Vector3 pos = transform.position;
+            pos.y = Mathf.Clamp(pos.y, minZoomValue, maxZoomValue);
+            transform.position = pos;
+        }
+    }
+}
